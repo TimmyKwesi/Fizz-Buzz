504d8484bd0177bf1def412ad8b8d665094ca490
TimmyKwesi <timmysteinx@gmail.com>
2023-12-31 12:42:16 +0000

    Update

--- contract/src/FizzBuzz.sol
+++ contract/src/FizzBuzz.sol
@@ -1,15 +1,17 @@
 pragma solidity 0.8.10;
 
 contract FizzBuzz {
+  // You can add more state variables here
 
-  function run(uint256 _upperBound) external pure returns(uint256) {
-    uint256 result = 0;
-    for (uint256 i = 0; i < _upperBound; i++) {
-      if (i % 3 == 0 || i % 5 == 0) {
-        result +=i; 
-      }
+    function run(uint256 _upperBound) external pure returns (uint256) {
+        _upperBound -= 1; // Exclude _upperBound from the sum
+
+        uint256 sumDivisibleBy = sumMultiples(_upperBound, 3) + sumMultiples(_upperBound, 5) - sumMultiples(_upperBound, 15);
+        return sumDivisibleBy;
     }
-    return result;
-  }
 
-}  
+    function sumMultiples(uint256 _upperBound, uint256 n) internal pure returns (uint256) {
+        uint256 p = _upperBound / n;
+        return n * (p * (p + 1)) / 2;
+    }
+}
