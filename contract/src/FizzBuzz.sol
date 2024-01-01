13bfb68d7043ca1a2271958a6fdbd63fde61f190
TimmyKwesi <timmysteinx@gmail.com>
2024-01-01 14:46:12 +0000

    Updated!

--- contract/src/FizzBuzz.sol
+++ contract/src/FizzBuzz.sol
@@ -1,7 +1,6 @@
 pragma solidity 0.8.10;
 
 contract FizzBuzz {
-
   function run(uint256 _upperBound) external pure returns (uint256) {
     unchecked {
       --_upperBound; // Exclude _upperBound from the sum
@@ -11,11 +10,10 @@ contract FizzBuzz {
       uint256 p15 = _upperBound / 15;
 
       // Combine calculations for efficiency:
-      uint256 sumDivisibleBy = 3 * (p3 * (p3 + 1)) / 2
-                              + 5 * (p5 * (p5 + 1)) / 2
-                              - 15 * (p15 * (p15 + 1)) / 2;
 
-      return sumDivisibleBy;
+      return ((3 * (p3 * (p3 + 1)) >> 1) + (5 * (p5 * (p5 + 1)) >> 1) - (15 * (p15 * (p15 + 1)) >> 1));
+
+      // return sumDivisibleBy
     }
   }
 }
